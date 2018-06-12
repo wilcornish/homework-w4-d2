4ca812c79b81a85774479e064df7b0b3d4218cb1
Wil Cornish <wilcoclahas@gmail.com>
2018-06-12 16:00:02 +0100

    refactored game logic, added controller

--- controller.rb
+++ controller.rb
@@ -1,4 +1,9 @@
 require('sinatra')
 require('sinatra/contrib/all')
 
-get('/:hand1/:hand2')
+require_relative('./models/rock_paper_scissors.rb')
+
+get('/:hand1/:hand2') do
+  game = RockPaperScissors.new(params[:hand1],params[:hand2])
+  game.play()
+end
--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -8,13 +8,19 @@ class RockPaperScissors
   end
 
   def play
-    return "paper wins" if (@hand1 == "rock" && @hand2 == "paper")
-    return "paper wins" if (@hand1 == "paper" && @hand2 == "scissors")
-    return "scissors wins" if (@hand1 == "paper" && @hand2 == "scissors")
-    return "scissors wins" if (@hand1 == "scissors" && @hand2 == "paper")
-    return "rock wins" if (@hand1 == "rock" && @hand2 == "scissors")
-    return "rock wins" if (@hand1 == "scissors" && @hand2 == "rock")
-    return "it's a draw"
+    win = {
+      "rock" => "scissors",
+      "paper" => "rock",
+      "scissors" => "paper"
+    }
+
+    if
+      @hand1 == @hand2; return "it's a draw"
+    elsif
+      win[@hand1] == @hand2; return "#{@hand1} wins"
+    else
+      "#{@hand2} wins"
+    end
   end
 
 end
